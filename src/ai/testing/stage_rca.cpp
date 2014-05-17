1659c80fba75b22b74626bb6ac55a8575168c67b
Kevin_Xi <kevin.xgr@gmail.com>
2014-05-17 10:20:20 +0800

    Add general dynamic programming part with simple scoring

--- src/ai/testing/stage_rca.cpp
+++ src/ai/testing/stage_rca.cpp
@@ -25,6 +25,10 @@
 #include "../composite/property_handler.hpp"
 #include "../gamestate_observer.hpp"
 #include "../../log.hpp"
+#include "../../resources.hpp"
+#include "../../tod_manager.hpp"
+#include "../../unit.hpp"
+#include "../../team.hpp"
 
 #include <boost/bind.hpp>
 #include <boost/foreach.hpp>
@@ -37,6 +41,11 @@ static lg::log_domain log_ai_testing_rca_default("ai/stage/rca");
 #define DBG_AI_TESTING_RCA_DEFAULT LOG_STREAM(debug, log_ai_testing_rca_default)
 #define LOG_AI_TESTING_RCA_DEFAULT LOG_STREAM(info, log_ai_testing_rca_default)
 #define ERR_AI_TESTING_RCA_DEFAULT LOG_STREAM(err, log_ai_testing_rca_default)
+
+/*void make_decision(int own_side_);
+void simulate_states_ahead(int own_side_, std::queue<turn_state> &states_);
+void set_optimal_policy(std::queue<turn_state> &states_);
+const turn_state simulate_state(const int own_side_, const int decision_no_, const turn_state &state_);*/
 
 candidate_action_evaluation_loop::candidate_action_evaluation_loop( ai_context &context, const config &cfg)
 	: stage(context,cfg)
@@ -93,6 +102,15 @@ bool candidate_action_evaluation_loop::do_play_stage()
 
 	//sort candidate actions by max_score DESC
 	std::sort(candidate_actions_.begin(),candidate_actions_.end(),desc_sorter_of_candidate_actions());
+
+	//Make general 'attack' or 'defence' decision for current turn
+	//utilizing dynamic programming.
+	int ticks = SDL_GetTicks();
+
+	make_decision();
+
+	int time_taken = SDL_GetTicks() - ticks;
+	LOG_AI_TESTING_RCA_DEFAULT <<"Took " << time_taken <<"ticks on decision making." << std::endl;
 
 	bool executed = false;
 	bool gamestate_changed = false;
@@ -202,6 +220,190 @@ rca_context& candidate_action_evaluation_loop::get_rca_context()
 
 candidate_action_evaluation_loop::~candidate_action_evaluation_loop()
 {
+}
+
+void candidate_action_evaluation_loop::make_decision()
+{
+	// Utilizing dynamic programming to find optimal policy
+
+	LOG_AI_TESTING_RCA_DEFAULT << "inside make_decision()"<< std::endl;
+
+	// First calculate state of current turn.
+	boost::shared_ptr<turn_state> stage_1(new turn_state(this->get_side(), resources::tod_manager->turn(), *resources::units, *resources::teams));
+	states_.push(*stage_1);
+
+	// Second calculate decisions based on current states.
+	simulate_states_ahead();
+
+	// Finally figure out the optimal policy for the total
+	// three turns and set the flag for CA to use.
+	set_optimal_policy();
+
+	LOG_AI_TESTING_RCA_DEFAULT << "------Analyze completed------" << std::endl;
+}
+
+void candidate_action_evaluation_loop::simulate_states_ahead()
+{
+	// Assume the final turn of dynamic programming is two
+	// turns ahead, that is, consider three turns totally.
+	const int total_turns = 3;
+	int current_turn = 1;
+
+	// Pop one state from queue and calculate resulted states
+	// of each decision, push them back to the queue.
+	while (current_turn < total_turns){
+		turn_state current_state = states_.front();
+		states_.pop();
+
+		for(int i = 0; i != decision::total_decisions; ++i){
+			states_.push(simulate_state(i, current_state));
+		}
+
+		current_turn = states_.front().get_turn_no();
+	}
+}
+
+
+void candidate_action_evaluation_loop::set_optimal_policy()
+{
+	// Find the final stage with maximum score.
+
+	turn_state optimal_final_state = states_.front();
+	states_.pop();
+	optimal_final_state.scoring_state();
+	double optimal_score = optimal_final_state.get_state_score();
+	decision optimal_decision = optimal_final_state.get_decision();
+
+	while(!states_.empty()){
+		turn_state current_final_state = states_.front();
+		states_.pop();
+		current_final_state.scoring_state();
+		double current_score = current_final_state.get_state_score();
+		if(current_score > optimal_score){
+			optimal_decision = current_final_state.get_decision();
+			optimal_score = current_score;
+		}
+	}
+
+	LOG_AI_TESTING_RCA_DEFAULT << optimal_decision << std::endl;
+
+	// TODO: Set flag.
+}
+
+const turn_state candidate_action_evaluation_loop::simulate_state(int decision_no_, turn_state &state)
+{
+	// Simulate CA based on state and get resulted turn_state.
+
+	const unit_map &units_ = state.get_units();
+	const std::vector<team> &teams_ = state.get_teams();
+
+	// TODO: Simulate.
+
+	// Construct new stage after execute CA.
+	boost::shared_ptr<turn_state> state_next(new turn_state(this->get_side(), state.get_turn_no()+1, units_, teams_));
+	const decision &decision_ = state.get_decision();
+	if(decision_.is_valid()){
+		state_next->set_decision(decision_.get_decision_no());
+	} else {
+		state_next->set_decision(decision_no_);
+	}
+
+	return *state_next;
+}
+
+// Implement class turn_state.
+turn_state::turn_state(const int own_side_, const int turn_no_, const unit_map &units_, const std::vector<team> &teams_) :
+	own_side_(own_side_),
+	turn_no_(turn_no_),
+	state_score_(0.0),
+	units_(units_),
+	teams_(teams_),
+	decision_(-1)
+{
+}
+
+turn_state::~turn_state()
+{
+}
+
+void turn_state::scoring_state(){
+	// A very simple scoring function.
+
+	const int total_team = teams_.size();
+	std::vector<double> state(total_team, 0.0);
+	std::vector<int> total_level(total_team, 0);
+
+	// Sum up the units' score.
+	for(unit_map::const_unit_iterator ui = units_.begin(); ui != units_.end(); ++ui) {
+		int current_side = ui->side() - 1;
+
+		double unit_score = (double)ui->hitpoints()/(double)ui->max_hitpoints()*(double)ui->cost();
+		LOG_AI_TESTING_RCA_DEFAULT << "\tside " << current_side+1 << " unit " << ui->type_name() << "'s score is " << unit_score << std::endl;
+		state[current_side] += unit_score;
+		total_level[current_side] += ui->level();
+	}
+
+
+	const int total_turns = 3;
+	const int turn_left = total_turns - turn_no_;
+	std::vector<int> upkeep_per_turn(total_team, 0);
+	std::vector<int> income_per_turn(total_team, 0);
+	std::vector<double> gold(total_team, 0.0);
+
+	// Add the gold that current have and that intend to
+	// get as income in future totally(subtract the upkeep),
+	// for each team.
+	for(std::vector<team>::const_iterator ti = teams_.begin(); ti != teams_.end(); ++ti) {
+		int current_side = ti->side() - 1;
+		upkeep_per_turn[current_side] = total_level[current_side]>ti->support() ? total_level[current_side]-ti->support() : 0;
+		income_per_turn[current_side] = ti->total_income() - upkeep_per_turn[current_side];
+
+		// The further, the rougher. So drop 0.1 weight for each turn.
+		gold[current_side] = ti->gold() + income_per_turn[current_side] * (1+(1-0.1*(double)(turn_left-1)))*turn_left/2;
+		state[current_side] += gold[current_side];
+
+		LOG_AI_TESTING_RCA_DEFAULT << "\tside " << current_side+1 << " will totally get " << gold[current_side] << " gold." << std::endl;
+	}
+
+
+	for(std::vector<team>::const_iterator ti = teams_.begin(); ti != teams_.end(); ++ti) {
+		int current_side = ti->side() - 1;
+		LOG_AI_TESTING_RCA_DEFAULT << "\tside " << current_side+1 << "'s state: " << state[current_side] << std::endl;
+		if(ti->is_enemy(own_side_)){ // 'is_enemy()' based on 1.
+			state_score_ -= state[current_side];
+		} else {
+			state_score_ += state[current_side];
+		}
+	}
+
+	LOG_AI_TESTING_RCA_DEFAULT << "State constructed with score " << state_score_ << std::endl;
+}
+
+// Implement class decision and overload operator<<.
+decision::~decision()
+{
+}
+
+std::ostream& operator<<(std::ostream &output, const decision &decision_)
+{
+	std::string policy = "";
+
+	switch(decision_.decision_no_){
+		case 0:
+			policy = "offensively";
+			break;
+		case 1:
+			policy = "defensively";
+			break;
+		default:
+			policy = "unknown";
+			break;
+	}
+
+	output << "The global optimal policy of this turn is to play "
+		<< policy << std::endl;
+
+	return output;
 }
 
 } // end of namespace testing_ai_default
--- src/ai/testing/stage_rca.hpp
+++ src/ai/testing/stage_rca.hpp
@@ -23,6 +23,8 @@
 #include "../composite/rca.hpp"
 #include "../composite/stage.hpp"
 
+#include <queue>
+
 #ifdef _MSC_VER
 #pragma warning(push)
 //silence "inherits via dominance" warnings
@@ -32,6 +34,9 @@
 namespace ai {
 
 namespace testing_ai_default {
+
+class decision;
+class turn_state;
 
 class candidate_action_evaluation_loop: public virtual stage, public virtual rca_context {
 public:
@@ -51,12 +56,80 @@ public:
 
 	void remove_completed_cas();
 
+	// Functions for dynamic programming.
+	void make_decision();
+
+	void simulate_states_ahead();
+
+	void set_optimal_policy();
+
+	const turn_state simulate_state(const int decision_no_, turn_state &state);
+
 private:
 	std::vector<candidate_action_ptr> candidate_actions_;
 
 	const config &cfg_;
+
+	std::queue<turn_state> states_;
 };
 
+class decision
+{
+	friend std::ostream& operator<<(std::ostream&, const decision&);
+
+private:
+	int decision_no_;
+
+public:
+	static const int total_decisions = 2; // We have two kinds of decisions now.
+
+	decision(int decision_no_) : decision_no_(decision_no_)
+	{
+	}
+
+	~decision();
+
+	int get_decision_no() const { return decision_no_; }
+
+	decision& set_decision_no(int decision_no_) { this->decision_no_ = decision_no_; return *this; }
+
+	bool is_valid() const { return (decision_no_ >= 0 && decision_no_ < total_decisions); }
+};
+
+class turn_state
+{
+private:
+	const int own_side_;
+
+	const int turn_no_;
+
+	double state_score_;
+
+	const unit_map units_;
+
+	const std::vector<team> teams_;
+
+	decision decision_;
+
+public:
+	turn_state(const int own_side_, const int turn_no_, const unit_map &units_, const std::vector<team> &teams_);
+
+	~turn_state();
+
+	void scoring_state();
+
+	int get_turn_no() const { return turn_no_; }
+
+	double get_state_score() const { return state_score_; } //Should I check this?
+
+	const unit_map& get_units() const { return units_; }
+
+	const std::vector<team>& get_teams() const { return teams_; }
+
+	const decision& get_decision() const { return decision_; }
+
+	void set_decision(int decision_no_) { decision_.set_decision_no(decision_no_); }
+};
 
 } // of namespace testing_ai_default
 
